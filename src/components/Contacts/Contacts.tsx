2c1fe6563d6a4bf23a2ef97468267a5f0a2abea9
Vladislav <melnikovv717@gmail.com>
2024-02-17 19:42:21 +0300

    add new animation

--- src/components/Contacts/Contacts.tsx
+++ src/components/Contacts/Contacts.tsx
@@ -1,8 +1,8 @@
 import { contacts } from '../../mock/contacts';
 import { motion } from 'framer-motion';
-import { Spaceship } from '../Spaceship/Spaceship';
 import styles from './Contacts.module.scss';
 import { mailIcon, tgLogo, linkedInLogo } from '../../assets/icons/icons';
+import { StarFox } from '../StarFox/StarFox';
 
 export const Contacts: React.FC = () => {
     const textAnimation = {
@@ -27,34 +27,34 @@ export const Contacts: React.FC = () => {
             className={styles.section}
         >
             <div className={styles.bg}>
-                <Spaceship />
+                <StarFox />
             </div>
 
             <div className={styles.text__wrapper}>
                 <motion.h2 variants={textAnimation}>Контакты</motion.h2>
-            </div>
 
-            <div className={styles.contacts__wrapper}>
-                <motion.div custom={1} variants={textAnimation} className={styles.contact}>
-                    <a href={contacts.mail} target='_blank' rel="noreferrer">
-                        <img src={mailIcon} alt='' />
-                    </a>
-                    <span>kivavladdev@yandex.ru</span>
-                </motion.div>
+                <div className={styles.contacts__wrapper}>
+                    <motion.div custom={1} variants={textAnimation} className={styles.contact}>
+                        <a href={contacts.mail} target='_blank' rel="noreferrer">
+                            <img src={mailIcon} alt='' />
+                        </a>
+                        <span>kivavladdev@yandex.ru</span>
+                    </motion.div>
 
-                <motion.div custom={2} variants={textAnimation} className={styles.contact}>
-                    <a href={contacts.telegram} target='_blank' rel="noreferrer">
-                        <img src={tgLogo} alt='' />
-                    </a>
-                    <span>@kivavlad</span>
-                </motion.div>
+                    <motion.div custom={2} variants={textAnimation} className={styles.contact}>
+                        <a href={contacts.telegram} target='_blank' rel="noreferrer">
+                            <img src={tgLogo} alt='' />
+                        </a>
+                        <span>@kivavlad</span>
+                    </motion.div>
 
-                <motion.div custom={3} variants={textAnimation} className={styles.contact}>
-                    <a href={contacts.linkedIn} target='_blank' rel="noreferrer">
-                        <img src={linkedInLogo} alt='' />
-                    </a>
-                    <span>in/kivavlad</span>
-                </motion.div>
+                    <motion.div custom={3} variants={textAnimation} className={styles.contact}>
+                        <a href={contacts.linkedIn} target='_blank' rel="noreferrer">
+                            <img src={linkedInLogo} alt='' />
+                        </a>
+                        <span>in/kivavlad</span>
+                    </motion.div>
+                </div>
             </div>
         </motion.section>
     )
